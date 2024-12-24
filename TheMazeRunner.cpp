0c70046122501e68826cbd188f4162fe752e6791
UmamaK47 <146324844+UmamaK47@users.noreply.github.com>
2024-12-24 13:05:10 +0500

    Update TheMazeRunner.cpp
    created game loop, console screen, and defined maze dimensions

--- TheMazeRunner.cpp
+++ TheMazeRunner.cpp
@@ -6,3 +6,25 @@
 using namespace sf;
 using namespace std;
 
+int main() {
+  //DEFINING MAZE DIMENSIONS
+    const int COLUMNS = 20;
+    const int ROWS = 10;
+    const int CELL_SIZE = 40;
+//SETTING CONSOLE SCREEN
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "THE MAZE RUNNER");
+    window.setFramerateLimit(60);
+  
+//GAME LOOP STARTS HERE
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape)) {
+                window.close();
+            }
+        }
+    }
+
+    return 0;
+}
